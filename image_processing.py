3eda82495b42eeff80ca6463a3850fe07cade699
Nguyen Duc Manh <manh16nd@gmail.com>
2019-11-23 17:14:29 +0700

    change algo

--- image_processing.py
+++ image_processing.py
@@ -1,20 +1,16 @@
 import cv2
+import numpy as np 
 
 
 def cal_value(gray):
-    n = 0
-    total = 0
-    print(len(gray))
-
-    for row in gray:
-        for cell in row:
-            n = n + 1
-            total = total + cell
-            print(n, total)
-
+    matrix = np.matrix(gray)
+    total = matrix.sum()
+    n = matrix.shape[0] * matrix.shape[1]
+    avarage_value = total / n
+    
     result = {
-        "total": total,
-        "avarage_value": total / n,
+        "total": matrix.sum(),
+        "avarage_value": avarage_value,
         "pixel_quantity": n
     }
 
@@ -25,4 +21,5 @@ def parse_img(file):
     print(file)
     img = cv2.imread(file)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
     return cal_value(gray)
